0fc8d6bdc1acf60f33b8132327ca364185e86136
Reinaldo Junior <juniorz@gmail.com>
2011-08-08 01:05:05 -0300

    Adds the sample Android project

---
+++ org.android.example.droid/src/org/android/example/droid/MainActivity.java
@@ -0,0 +1,14 @@
+package org.android.example.droid;
+
+import org.android.example.droid.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

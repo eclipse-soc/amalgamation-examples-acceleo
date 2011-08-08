4d469109f5435eca399b35288238dd4420f46d01
Reinaldo Junior <juniorz@gmail.com>
2011-08-08 18:27:16 -0300

    Generates code to the Screen concept

--- org.android.example.droid/src-gen/org/android/example/droid/MainActivity.java
+++ org.android.example.droid/src-gen/org/android/example/droid/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+		setContentView(R.layout.main);
     }
 }
